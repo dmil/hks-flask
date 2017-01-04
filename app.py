709cb4448569306dec1022332aab8999aa2b84c3
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2017-01-03 23:08:37 -0500

    use port env var

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,5 +8,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    PORT = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=PORT)
 
